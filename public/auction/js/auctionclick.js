413223de904d46fa2784139e21624e393da4b8de
yjbae3974 <brianbae6@korea.ac.kr>
2021-05-26 22:51:43 +0900

    .

--- public/auction/js/auctionclick.js
+++ public/auction/js/auctionclick.js
@@ -1,29 +1,47 @@
+var usableCash = 300000; //가정, 현재 사용가능한 캐쉬 여기다가 저장.
 var ipchalCash = 25500; // 시작은 제작사가 설정한 최소 가격으로 시작
-var rate = 0.1;
-document.getElementById("ipchal").innerText = ipchalCash;
-document.getElementById("ipchalRate").innerText = rate;
+var rate = 1;
+var costTotal = ipchalCash * rate;
+document.getElementById("ipchal").innerText = ipchalCash.toLocaleString('ko-KR');
+document.getElementById("ipchalRate").innerText = rate/10;
+document.getElementById("usableCash").innerText = usableCash.toLocaleString('ko-KR');
+document.getElementById("costTotal").innerText = costTotal.toLocaleString('ko-KR');
 function plusCash(){
     ipchalCash+=500;
-    document.getElementById("ipchal").innerText = ipchalCash;
+    document.getElementById("ipchal").innerText = ipchalCash.toLocaleString('ko-KR');
+    costTotal = ipchalCash * rate;
+    document.getElementById("costTotal").innerText = costTotal.toLocaleString('ko-KR');
 }
 function minusCash(){ //이것도 제작자가 설정한 최소가격으로는 내려가지 않게
     ipchalCash-=500;
-    document.getElementById("ipchal").innerText = ipchalCash;
+    document.getElementById("ipchal").innerText = ipchalCash.toLocaleString('ko-KR');
+    costTotal = ipchalCash * rate;
+    document.getElementById("costTotal").innerText = costTotal.toLocaleString('ko-KR');
 }
 function plusRate(){
-    rate +=0.1;
-    
-    document.getElementById("ipchalRate").innerText = rate.toFixed(1);
+    rate +=1;
+    document.getElementById("ipchalRate").innerText = rate/10;
+    costTotal = ipchalCash * rate;
+    document.getElementById("costTotal").innerText = costTotal.toLocaleString('ko-KR');
 
 }
 function minusRate(){
-    if(rate<0.2){
+    if(rate<2){
         alert("수량은 0.1% 이하로 내려갈 수 없습니다!");
     }
     else{
-        rate -=0.1;
-    document.getElementById("ipchalRate").innerText = rate.toFixed(1);
+        rate -=1;
+    document.getElementById("ipchalRate").innerText = rate/10;
+    costTotal = ipchalCash * rate;
+    document.getElementById("costTotal").innerText = costTotal.toLocaleString('ko-KR');
     }
     
 }
-//일단 여기까지 하고 이제 해야할 거는 그 가격 계산이랑 버튼누르면 더 되는거.
+function addRate(stock){
+    rate+=stock;
+    document.getElementById("ipchalRate").innerText = rate/10;
+    costTotal = ipchalCash * rate;
+    document.getElementById("costTotal").innerText = costTotal.toLocaleString('ko-KR');
+
+}
+
